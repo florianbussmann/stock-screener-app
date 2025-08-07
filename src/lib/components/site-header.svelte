d21439d2ef67e8229c3c70aa76524d763e90dbf2
Florian Bussmann <hallo@florian-bussmann.de>
2025-08-07 12:26:20 +0000

    feat: breadcrumb

--- src/lib/components/site-header.svelte
+++ src/lib/components/site-header.svelte
@@ -5,6 +5,29 @@
     import { Kbd } from "$lib/components/ui/kbd";
     import CommandIcon from "@lucide/svelte/icons/command";
 
+    import { onMount } from "svelte";
+    import { afterNavigate } from "$app/navigation";
+    import { page } from "$app/state";
+    import { menuItems } from "$lib/menu.js";
+    import { findBreadcrumb } from "$lib/breadcrumb.js";
+    import * as Breadcrumb from "$lib/components/ui/breadcrumb/index.js";
+
+    // Store breadcrumb in reactive variable
+    let breadcrumb: string | any[] = [];
+    let defaultBreadcrumb = [{ name: "Home", path: "/" }];
+
+    // On initial mount
+    onMount(() => {
+        breadcrumb =
+            findBreadcrumb(page.route.id || "", menuItems) || defaultBreadcrumb;
+    });
+
+    // On client-side navigation
+    afterNavigate(() => {
+        breadcrumb =
+            findBreadcrumb(page.route.id || "", menuItems) || defaultBreadcrumb;
+    });
+
     import { searchOpen } from "$lib/stores/search";
 </script>
 
@@ -17,6 +40,25 @@
             orientation="vertical"
             class="mx-2 data-[orientation=vertical]:h-4"
         />
+        <Breadcrumb.Root>
+            <Breadcrumb.List>
+                {#each breadcrumb as item, i}
+                    {#if i < breadcrumb.length - 1}
+                        <Breadcrumb.Item class="hidden md:block">
+                            <Breadcrumb.Link href="{item.path}"
+                                >{item.name}</Breadcrumb.Link
+                            >
+                        </Breadcrumb.Item>
+                        <Breadcrumb.Separator class="hidden md:block" />
+                    {:else}
+                        <title>Stock Screener - {item.name}</title>
+                        <Breadcrumb.Item>
+                            <Breadcrumb.Page>{item.name}</Breadcrumb.Page>
+                        </Breadcrumb.Item>
+                    {/if}
+                {/each}
+            </Breadcrumb.List>
+        </Breadcrumb.Root>
 
         <div class="ml-auto flex items-center gap-2">
             <Button variant="secondary" onclick={() => searchOpen.set(true)}
